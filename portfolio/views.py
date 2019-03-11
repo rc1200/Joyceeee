2f5fc91c105996029692418a7ac9fccefbd3b155
rc1200 <ron.calibuso@gmail.com>
2019-03-11 01:20:09 -0400

    split all the elements to it's own seperate pages

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, "portfolio/index.html")
+
+
+# include templates
+def header(request):
+    return render(request, "portfolio/header.html")
+
+
+def myCarousel(request):
+    return render(request, "portfolio/myCarousel.html")
+
+
+def marketing(request):
+    return render(request, "portfolio/marketing.html")
+
+
+def myCarousel(request):
+    return render(request, "portfolio/myCarousel.html")
+
+
+def featurettes(request):
+    return render(request, "portfolio/featurettes.html")
+
+
+def footer(request):
+    return render(request, "portfolio/footer.html")
