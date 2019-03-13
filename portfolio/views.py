72caf8423f229d6a65a16a40384e9675d9b1f120
rc1200 <ron.calibuso@gmail.com>
2019-03-12 23:05:22 -0400

    loop through carousel, static context dictionary

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,7 +5,19 @@ def index(request):
     return render(request, "portfolio/index.html")
 
 
+def index2(request):
+    # carousel_img = {'c_imgs': ['assets/img/c1.jpg', 'assets/img/c2.jpg', 'assets/img/c3.jpg'],
+    # 'person': [{'nn': 'ron', 'xx':'ronx'}, {'nn': 'aidan', 'xx':'ronyy'}]}
+    carousel_stuff = {'stuff': [{'img': 'assets/img/c1.jpg', 'heading': 'heading1', 'is_active': 'active'},
+                                {'img': 'assets/img/c2.jpg', 'heading': 'heading2', 'is_active': ''},
+                                {'img': 'assets/img/c3.jpg', 'heading': 'heading3', 'is_active': ''}
+                                ]}
+
+    return render(request, "portfolio/index2.html", carousel_stuff)
+
 # include templates
+
+
 def header(request):
     return render(request, "portfolio/header.html")
 
