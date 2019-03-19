52c1b7fac007758078e761da282b80541700cd29
rc1200 <ron.calibuso@gmail.com>
2019-03-19 00:56:39 -0400

    cleaned up models field, don't need to hardcode images filename anymore

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,12 +3,10 @@ from django.db import models
 
 class Carousel(models.Model):
     heading = models.CharField(max_length=200)
-    # carousel_num = models.CharField(max_length=11, null=True, blank=True)
     carousel_num = models.IntegerField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True, width_field='width_field', height_field='height_field')
     height_field = models.IntegerField(default=0, null=True, blank=True)
     width_field = models.IntegerField(default=0, null=True, blank=True)
-    img = models.CharField(max_length=200)
     is_active = models.CharField(max_length=10, null=True, blank=True)
     text = models.TextField()
     text_align = models.CharField(max_length=20, default='text-right', null=True, blank=True)
